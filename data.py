714d9625e27f31df020f450f07c125ab661f2fdf
sᴜᴍɪᴛ <105429103+issu-op@users.noreply.github.com>
2022-08-10 14:04:19 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("🌚 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🌚", callback_data="generate")]
 
     generate_button = [generate_single_button]
 
