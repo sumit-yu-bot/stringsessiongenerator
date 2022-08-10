0c86e0ea553d9d86ed34b39ed38095d30ac42956
sᴜᴍɪᴛ <105429103+issu-op@users.noreply.github.com>
2022-08-10 14:03:14 +0530

    Update data.py

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ class Data:
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/lockroom"),
-         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/ab_sumit"),
+        [InlineKeyboardButton("❤️‍🔥 sᴜᴩᴩᴏʀᴛ ❤️‍🔥", url="https://t.me/lockroom"),
+         InlineKeyboardButton("🕶 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🕶 ", url="https://t.me/ab_sumit"),
         ],
     ]
 
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/issu-op/stringgenerator)
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/issu-op/stringgeneratorbot)
 Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ѕυмιт](https://t.me/Kya_rakhu_smjh_nhi_aa_rha) !
     """
