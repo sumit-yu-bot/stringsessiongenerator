92d752b82b9eab7c127467a75d47ab22e580ca1b
isu-op-op <114138818+isu-op-op@users.noreply.github.com>
2022-09-25 12:58:20 +0530

    Update data.py

--- data.py
+++ data.py
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/issu-op/stringgeneratorbot)
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/isu-op-op/stringsessiongenerator)
 Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ѕυмιт](https://t.me/Kya_rakhu_smjh_nhi_aa_rha) !
     """
